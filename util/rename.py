bd0fd692b9cc2e2094e6e8899b0bab8f249035c1
Nathan Baltzell <baltzell@lappi4.mynetworksettings.com>
2023-08-10 20:01:13 -0400

    finish rename

--- util/rename.py
+++ util/rename.py
@@ -1,19 +1,35 @@
-#!/usr/bin/env  python3
+#!/usr/bin/env python3
 
 import os,re
 
+moves = []
+links = []
+
 for d,subdirs,files in os.walk('./coatjava'):
-  for file in files:
-    if not file.endswith('.yaml'):
-      continue
-    m = re.match('.*(_pass[12][v12]*).*',file)
-    if m is None:
-      continue
-    os.path.normpath(
-    oldfile = file
-    newfile = file.replace(m.group(1),'')
-    if os.path.islink(oldfile):
-      print(oldfile)
-    else:
-      print('NO')
+
+    for file in files:
+
+        if not file.endswith('.yaml'):
+            continue
+
+        match = re.match('.*(_pass[12v_]).*',file)
+
+        old_file = os.path.join(d,file)
+        new_file = old_file.replace(match.group(1),'')
+
+        if os.path.islink(old_file):
+
+            old_dest = os.path.realpath(old_file)
+            old_dest = old_dest[old_dest.find('coatjava'):]
+            match = re.match('.*(_pass[12v_]).*',old_dest)
+            new_dest = old_dest.replace(match.group(1),'')
+            links.append('git rm %s'%(old_file))
+            links.append('ln -s %s %s'%(new_file, new_dest))
+            links.append('git add %s'%(new_file))
+
+        else:
+            moves.append('git mv %s %s'%(old_file,new_file))
+
+print('\n'.join(moves))
+print('\n'.join(links))
 
