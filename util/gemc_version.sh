5d19dfdd57caefd42ad87c2d6eedf2f586bb0832
mungaro <maurizio.ungaro@cnu.edu>
2023-10-27 09:16:36 -0400

    adding version to rgm gcards

--- util/gemc_version.sh
+++ util/gemc_version.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# check if the directory gemc exists, if not print message
+if [ ! -d "gemc" ]; then
+	echo "ERROR: Run this script at the top of the repository: util/gemc_version.sh"
+	exit 1
+fi
+
 # the string <!-- GEMC dev --> is replaced with the current version for all new tags
 for x in $(find gemc -type f -name '*.gcard')
 do
